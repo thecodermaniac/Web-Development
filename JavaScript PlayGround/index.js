cdf033252d49df645ebf0983aa99a93b7349c5f7
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-10-18 11:42:18 +0530

    new changes

--- JavaScript PlayGround/index.js
+++ JavaScript PlayGround/index.js
@@ -16,9 +16,48 @@
 // }
 // console.log(arr);
 
+
+
 var inp = [{ a: 1 }, { b: 2 }, { c: 3 }]
 var dict = {}
 inp.map((val) => {
     dict[Object.keys(val)] = Object.values(val)
 })
 console.log(dict);
+
+import school from './list.json' assert { type: 'json' };
+
+console.log(school);
+
+let MappedList = school.map((student) => {
+    let name = student.name;
+    let subject = student.subject;
+    return ({ name, subject })
+})
+console.log(MappedList);
+
+let FilteredList = school.filter((student) => {
+    return student.age > 13
+})
+
+console.log(FilteredList);
+
+let ReducedList = school.reduce((acc, student) => {
+    if (acc[student.school]) {
+        acc[student.school] = ++acc[student.school]
+    }
+    else {
+        acc[student.school] = 1;
+    }
+    return acc;
+}, {})
+
+console.log(ReducedList);
+
+function transformRes(res) {
+    const jsonStartingChars = ["{", "["];
+    return jsonStartingChars.includes(res.charAt(0)) ? JSON.parse(res) : res;
+}
+
+let result = transformRes('{key:Aritra}')
+console.log(result);
--- Prac_1/prac_1.js
+++ Prac_1/prac_1.js
@@ -7,6 +7,7 @@ var element = document.getElementsByTagName("img");
 console.log(element);
 created_element = document.createElement("img");
 created_element.setAttribute("style", "display: none");
+console.log(document.getElementById('veryunique'));
 for (let i = 0; i < element.length; i++) {
   element_img = element[i];
   element_img.addEventListener("click", function () {
